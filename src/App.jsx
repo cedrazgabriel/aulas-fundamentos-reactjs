2f42325178f664af923f410dc8e710137c5483f6
Gabriel Cedraz <69488235+cedrazgabriel@users.noreply.github.com>
2022-09-22 22:17:42 -0300

    Update Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Post } from "./Post"
+import { Post } from "./components/Post"
 import { Header } from "./components/Header"
 import { Sidebar } from "./components/Sidebar"
 
@@ -22,11 +22,6 @@ import './global.css'
           author="Gabriel Cedraz"
           content="Uma frase legal"
           />
-
-         <Post
-         author="Lucas Inutilismo"
-         content="Outra frase legal"
-         />
       </main>
      </div>
     </div>
