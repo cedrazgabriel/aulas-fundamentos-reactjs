108fa282e9b8483cdc33410deac420c3196f0dbc
Gabriel Cedraz <cedrazdev@gmail.com>
2024-06-14 08:57:15 -0300

    feat: create avatar component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,12 @@
+import { Avatar } from './Avatar'
 import { ThumbsUp, Trash } from 'phosphor-react'
+
 import styles from './Comment.module.css'
 
 export function Comment() {
     return(
         <div className={styles.comment}>
-            <img src="https://github.com/cedrazgabriel.png" alt="Logo" />
+            <Avatar src="https://github.com/cedrazgabriel.png"/>
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                     <header>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css'
 import {PencilLine} from 'phosphor-react'
 
@@ -9,9 +10,7 @@ export function Sidebar() {
             />
             
            <div className={styles.profile}>
-           <img
-            className={styles.avatar}
-            src="https://github.com/cedrazgabriel.png" />
+            <Avatar src="https://github.com/cedrazgabriel.png"/>
             <strong>Gabriel Cedraz</strong>
             <span>Web Developer</span>
             <footer>
